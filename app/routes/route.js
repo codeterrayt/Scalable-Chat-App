cdc35f1a8265948d10f4c29516acb1cd6d3eadda
Code Terra <rp.codeterra@gmail.com>
2024-02-04 17:26:30 +0530

    Converted to API

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,37 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const redis = require('ioredis');
-const redisNor = new redis();
-const Message = require("../db/schema")
+const { fetchMessagesFromRedis } = require("../redis/redis")
 
-const fetchMessagesFromRedis = async ()=>{
-    return await redisNor.lrange("messages",0,-1)
-} 
-
+router.get("/get_messages", async (req, res) => {
+    let msg = await fetchMessagesFromRedis();
+    res.json(msg);
+});
 
 router.get("/", async (req, res) => {
-    
-    let msg = await fetchMessagesFromRedis();
-    let message = msg? msg : [];
-    // console.log(message)
-
-    let first_message = [];
-
-    if(message.length !== 0){
-        first_message = JSON.parse(message[0]);
-        message.pop();
-        console.log(first_message)
-
-        // let msg = await Message.$where({$lte: first_message.createdAt});
-        // console.log(msg)
-
-    }
-
-    
-
-    return res.render("index", {
-        message, first_message
-    });
+    return res.json({ status: true });
 });
 
 
