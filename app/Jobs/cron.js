7f57ceb091196bcd755422a3f17a332b29147122
Code Terra <rp.codeterra@gmail.com>
2024-02-04 17:17:06 +0530

    Cron Jobs for Sync the Redis and DB data

---
+++ app/Jobs/cron.js
@@ -0,0 +1,21 @@
+var cron = require('node-cron');
+const Message = require("../db/schema");
+const { fetchMessagesFromRedis, updateMessagesInRedis } = require("../redis/redis")
+
+cron.schedule('* 1 * * * *', async () => {
+
+    try {
+        console.log('Running a task every minute');
+
+        // Fetch data from MongoDB
+        const messagesFromMongoDB = await Message.find({});
+
+        // Update Redis with the fetched data
+        await updateMessagesInRedis(messagesFromMongoDB);
+
+        console.log('Data synchronized to Redis successfully');
+    } catch (error) {
+        console.error('Error synchronizing data to Redis:', error);
+    }
+
+});
