da8439497c23598caa97397f2459bf872a2d62cb
Code Terra <rp.codeterra@gmail.com>
2024-02-04 17:22:11 +0530

    added socket_id, Transform to hide _id

--- app/db/schema.js
+++ app/db/schema.js
@@ -1,8 +1,15 @@
 const mongoose = require("./db");
 
 const messageSchema = new mongoose.Schema({
+    socket_id: { type: String },
     message: { type: Object, required: true },
-    createdAt: {type: Date, default: Date.now}
+    createdAt: { type: Date, default: Date.now }
+}, {
+    toJSON: {
+        transform: function (doc, ret) {
+            delete ret._id; // Exclude the _id field
+        }
+    }
 });
 
 const Message = mongoose.model('Message', messageSchema);
